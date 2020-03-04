d7b629cc866c324048ed7d7f6ae5454f4ffc7b68
SaimonMR <simon.martinez.rzs@gmail.com>
2020-03-04 13:00:32 +0100

    First commit

---
+++ scripts/setup_ubuntu_18_04.sh
@@ -0,0 +1,28 @@
+read -p "Setting Up Ubuntu. Press a ENTER to contine. (CTRL+C) to cancel"
+
+#! /bin/bash
+
+# Installing ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install ros-melodic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+# Installing Gazebo
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+cat /etc/apt/sources.list.d/gazebo-stable.list
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gazebo9
+sudo apt-get install libgazebo9-dev
+
+
+# Gazebo Dependencies
+sudo apt upgrade libignition-math2
+
+
